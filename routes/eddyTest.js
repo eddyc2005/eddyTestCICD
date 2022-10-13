8abaebfe1bec661889a2eb161accbe7b55494343
eddy.chu <eddy.chu@104.com.tw>
2022-10-13 15:10:27 +0800

    test

--- routes/eddyTest.js
+++ routes/eddyTest.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/test', function(req, res, next) {
-  res.send('test success!');
+  res.send('test success!!');
 });
 
 module.exports = router;
