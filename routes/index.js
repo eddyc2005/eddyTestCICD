8fabc266d6564470ab8e6bde5041cc61a7d7c6ca
eddy.chu <eddy.chu@104.com.tw>
2022-10-20 17:04:07 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
+  res.send('Healthy!!');
+});
+
+/* GET home page. */
+router.get('/eddyTest', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/test', function(req, res, next) {
+router.get('/eddyTest/test', function(req, res, next) {
   res.send('test success!!!');
 });
 
